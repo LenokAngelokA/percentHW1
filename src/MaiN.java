a05870292dad4ed8763b6355c4f15bb1f899789d
Elena <arhangelskaya.lena@mail.ru>
2024-04-05 17:11:47 +0300

    First commit

---
+++ src/MaiN.java
@@ -0,0 +1,10 @@
+public class MaiN {
+    public static void main(String[] args) {
+        int amount = 22880;
+        int bonus = 20;
+        int cashback = amount * bonus / 100;
+
+        System.out.println("Итоговый бонус" + cashback);
+    }
+    int present = 4576;
+}
